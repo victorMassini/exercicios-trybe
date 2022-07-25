228e5575f47a831f5e1ade232c271c37f47a3f12
Victor Massini <vmprata@gmail.com>
2022-07-25 20:05:43 -0300

    Js

---
+++ fundamentos/bloco-04-intrudocao-a-javascript/primeiro-exercicio/primeiro-contato.js
@@ -0,0 +1,21 @@
+let info = {
+    personagem: 'Margarida e',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+  }
+ for (index in info) {
+    
+ }
+
+
+  let obj2 = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+  }
+ for (index in obj2) {
+    console.log(obj2[index])
+ }
+  console.log("Bem-vinda, "+ info.personagem)
